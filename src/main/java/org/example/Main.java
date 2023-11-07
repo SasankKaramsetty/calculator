165c2321fc4e1cf6b90d1a8582d11cebc23e8b45
SasankKaramsetty <karamsettysasank@gmail.com>
2023-11-07 23:02:55 +0530

    changes in code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,8 @@ public class Main {
         while(true)
         {
             System.out.println("operation");
-            System.out.println("1 +");
+            
+            System.out.println("1 addition ");
             System.out.println("2 -");
             System.out.println("3 *");
             System.out.println("4 /");
